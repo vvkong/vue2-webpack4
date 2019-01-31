5b487bc03fbb209e01f15b61b933fddec9f21a84
wangrenxing@cnhnkj.com <wangrenxing@cnhnkj.com>
2019-01-31 20:50:50 +0800

    各种插件安装配置

---
+++ .eslintrc.js
@@ -0,0 +1,16 @@
+module.exports = {
+    extends: [
+      // add more generic rulesets here, such as:
+      // 'eslint:recommended',
+      // 'plugin:vue/recommended',
+      "standard",
+      "plugin:vue/essential",
+    ],
+    rules: {
+      // override/add rules settings here, such as:
+      // 'vue/no-unused-vars': 'error'
+      'no-console': 'off',
+    },
+    "plugins": [
+    ]
+  }
--- src/components/app.vue
+++ src/components/app.vue
@@ -1,27 +1,67 @@
 <template>
-    <div>
-        <h2>{{title}}</h2>
-        <!-- 新增 -->
-        <div class="test-prefix">
-            <p>我就测试一下，看看而已</p>
-        </div>
-        <img src="../assets/images/logo.png" width="50px" height="50px"/>
-        <div class="bg">
-            我在背景图上
-        </div>
+  <div>
+    <h2>{{ title }}</h2>
+    <div class="test-prefix">
+      <p>我就测试一下，看看而已，我是红色哦</p>
     </div>
+    <img
+      src="../assets/images/logo.png"
+      width="50px"
+      height="50px"
+    />
+    <div class="bg">
+      我在背景图上
+    </div>
+    <button v-bind:click="onClickButton">
+      测试ES6语法
+    </button>
+    <div v-for="(item, idx) in list" :key="idx">
+      <p>{{item}}{{}}</p>
+    </div>
+  </div>
 </template>
 <script>
+class Echo {
+  constructor (msg) {
+    this.msg = msg
+  }
+  print () {
+    console.log(this.msg)
+  }
+  getMsg () {
+    return this.msg
+  }
+}
 export default {
-    data() {
-        return {
-            title: 'Hello vue2&&webpack4....3'
-        }
+  data () {
+    return {
+      title: 'Hello vue2&&webpack4....3',
+      list: ['test1', 'test2', 'test3']
     }
+  },
+  methods: {
+    onClickButton (e) {
+      let echo = new Echo('我是ES6语法class构建的对象哦')
+      echo.print()
+      alert(echo.getMsg())
+      let sum = 2 ** 10
+      alert(`求幂运算符（2 ** 10）= ${sum}`)
+    },
+    async getA () {
+      return new Promise(new function (resolve, rejuect) {
+        console.log('====== getA')
+        setTimeout(function () {
+          resolve(`It's timeout.`)
+        }, 3000)
+      }())
+    },
+    $test () {
+
+    }
+  }
 }
 </script>
 <style scoped>
-    @import url('../styles/global.css');
     .bg {
         width: 500px;
         height: 200px;
@@ -31,5 +71,6 @@ export default {
     .test-prefix{
         display: flex;
         user-select: none;
+        color: red;
     }
 </style>
